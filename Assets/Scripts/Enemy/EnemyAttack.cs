f1565c48641e888092a2ea3a0680a2fc6528df20
Lê Nguyễn Minh Hiếu <minhhieu.le>
2022-12-12 18:23:03 +0700

    Init project

---
+++ Assets/Scripts/Enemy/EnemyAttack.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using BehaviorDesigner.Runtime;
+using BehaviorDesigner.Runtime.Tasks;
+
+public class EnemyAttack : MonoBehaviour
+{
+    public float timeBetweenAttacks = 0.5f;
+    public int attackDamage = 10;
+
+    public SharedGameObject sharedGameObject;
+    public SharedFloat sharedSpeed;
+
+    GameObject player;
+    Animator anim;
+    PlayerHealth playerHealth;
+    EnemyHealth enemyHealth;
+    BehaviorTree behaviorTree;
+
+    void Awake ()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerHealth = player.GetComponent <PlayerHealth> ();
+        enemyHealth = GetComponent<EnemyHealth>();
+        anim = GetComponent <Animator> ();   
+    }
+
+    private void Start()
+    {
+        behaviorTree = GetComponent<BehaviorTree>();
+        sharedGameObject.Value = player;
+        behaviorTree.SetVariable("Target", sharedGameObject);
+        behaviorTree.RegisterEvent("AttackPlayer", Attack);
+        behaviorTree.RegisterEvent<float>("SetSpeed", SetSpeed);
+    }
+
+    void Update()
+    {
+        if (playerHealth.currentHealth <= 0)
+        {
+            anim.SetTrigger("PlayerDead");
+        }
+    }
+
+    void Attack()
+    {
+        if (enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0)
+        {
+            playerHealth.TakeDamage(attackDamage);
+        }
+    }
+
+    void SetSpeed(float speed)
+    {
+        sharedSpeed.Value = speed;
+        behaviorTree.SetVariable("MovingSpeed", sharedSpeed);
+    }
+}
